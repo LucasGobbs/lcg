4bdb5a9bdc5da122899349886fab2ebb3dd7f421
LucasGobbs <49127170+LucasGobbs@users.noreply.github.com>
2020-05-13 11:13:17 -0300

    novas funcoes

--- lcg_glal/glal.h
+++ lcg_glal/glal.h
@@ -36,7 +36,7 @@ GLAL_MATRIX_TYPE(Mat4,4,4);
 // Create Simple matrix
 #define Mat_create_def(name, rows, colums) name GLAL_CONCAT(name, _create)()
 #define Mat_create_impl(name, rows, colums) Mat_create_def(name, 4, 4){\
-	name a = { 0 }; return a; \
+	name a = { { 0 }  }; return a; \
 }\
 
 // Print matrix
@@ -78,7 +78,7 @@ GLAL_MATRIX_TYPE(Mat4,4,4);
 #define Mat_create_identity_def(name, rows, colums) name GLAL_CONCAT(name, _create_identity)()
 #define Mat_create_identity_impl(name, rows, colums) Mat_create_identity_def(name, rows, colums){\
 	int i;\
-	name mat = { 0 };\
+	name mat = { {0} };\
 	FOREACH(rows, i){\
 		MGET(mat, i, i) = 1.0;\
 	}\
@@ -87,11 +87,21 @@ GLAL_MATRIX_TYPE(Mat4,4,4);
 
 #define Mat_create_copy_def(name, rows, colums) name GLAL_CONCAT(name, _create_copy)(name mat)
 #define Mat_create_copy_impl(name, rows, colums) Mat_create_copy_def(name, rows, colums){\
-	name rt = { 0 };\
-	rt.data = mat.dat;\
+	name rt = { {0} };\
 	return rt;\
 }\
 
+#define Mat_create_fill_def(name, rows, colums) name GLAL_CONCAT(name, _create_fill)(TYPE value)
+#define Mat_create_fill_impl(name, rows, colums) Mat_create_fill(name, rows, colums){\
+	name rt = { { 0 } };\
+	int i, j;\
+	FOREACH(rows, i){\
+		FOREACH(colums, j){\
+			MGET(rt, i, j) = value;\
+		}\
+	}\
+	return rt;\
+}\
 // TODO Mat_create #
 // TODO Mat_create_identity #
 // TODO Mat_create_copy
@@ -110,10 +120,11 @@ GLAL_MATRIX_TYPE(Mat4,4,4);
 
 // TODO Mat_print #
 // TODO Mat_prints #
-
+/*
 void mat_add(Mat2* mata, Mat2 matb ){
 	(*mata).data[0][0] += matb.data[0][0];
 }
+*/
 // TODO Mat_add
 // TODO Mat_sub
 // TODO Mat_factor
@@ -129,6 +140,16 @@ EXPAND_DEF(Mat_prints_def)
 
 EXPAND_DEF(Mat_create_identity_def)
 
+EXPAND_DEF(Mat_create_copy_def)
+
+#define Mat_print(mat) \
+	if(__builtin_types_compatible_p(__typeof__(mat), struct Mat2)){\
+		Mat2_print(mat);\
+	} else {\
+		printf("aaa");\
+	}\
+
+
 #ifdef GLAL_IMPLEMENTATION
 //=================================================================================================================
 // START IMPLEMENTATIOn
@@ -139,6 +160,8 @@ EXPAND_IMPL(Mat_print_impl)
 EXPAND_IMPL(Mat_prints_impl)
 
 EXPAND_IMPL(Mat_create_identity_impl)
+
+EXPAND_IMPL(Mat_create_copy_impl)
 
 #endif //GLAL_IMPLEMENTATION
 
--- lcg_glal/glal_example.c
+++ lcg_glal/glal_example.c
@@ -3,7 +3,8 @@
 #define GLAL_IMPLEMENTATION
 #include "glal.h"
 
-//gcc -Wall -pedantic -std=c99 -o exe example.c -lm
+// gcc -Wall -pedantic -std=c99 -o exe glal_example.c -lm
+// gcc -Wno-missing-braces -Wall -pedantic -std=c99 -o exe glal_example.c -lm
 void create();
 int main(){
 	create();
@@ -14,13 +15,14 @@ void create(){
 	Mat2 a = Mat2_create_identity();
 	Mat3 b = Mat3_create();
 
-	Mat4 c = Mat4_create();
-	Mat4 d = { 0 };
+	Mat2 c = Mat2_create_copy(a);
+	Mat4 d = { {0} };
 
-	c = Mat4_create_identity();
-	mat_add(&a, Mat2_create_identity());
+	//c = Mat4_create_identity();
+	//mat_add(&a, Mat2_create_identity());
 	Mat2_print(a);
 	Mat3_print(b);
-	Mat4_prints(c, "c");
+	Mat2_prints(c, "c");
 	Mat4_prints(d, "d");
+	Mat_print(b)
 }
