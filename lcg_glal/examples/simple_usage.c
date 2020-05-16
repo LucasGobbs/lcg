e1f0403b4dc4b8652092ab68b1e7a5d05149b646
LucasGobbs <llcostagobbi@gmail.com>
2020-05-15 22:12:07 -0300

    Adicionando pasta de exemplos

--- lcg_glal/examples/simple_usage.c
+++ lcg_glal/examples/simple_usage.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
 #define GLAL_PREFIX a_
 #define GLAL_IMPLEMENTATION
-#include "glal.h"
-
+#include "../glal.h"
 // gcc -Wall -pedantic -std=c99 -o exe glal_example.c -lm
 // gcc -Wno-missing-braces -Wall -pedantic -std=c99 -o exe glal_example.c -lm
 float identity_mult_scalar(size_t i, size_t j, size_t x);
-void simple_usage();
 int main(){
-	simple_usage();
-	return 0;
-}
-void simple_usage(){
 	a_Mat2 mat2 = a_Mat2_create();
 	a_Mat3 mat3 = a_Mat3_create_identity();
 	a_Mat4 mat4 = a_Mat4_create_fill(2.0);
@@ -24,6 +18,8 @@ void simple_usage(){
 	a_Mat4_prints(copy, "Copy Mat4");
 
 	a_Mat4_prints(other_mat4, "Identity * scalar");
+	
+    return 0;
 }
 float identity_mult_scalar(size_t i, size_t j, size_t x){
 	if(i==j){
@@ -32,3 +28,31 @@ float identity_mult_scalar(size_t i, size_t j, size_t x){
 		return 0;
 	}
 }
+/* outputs
+	Mat2
+	0       0
+	0       0
+
+	Mat3
+	1.000   0       0
+	0       1.000   0
+	0       0       1.000
+
+	Original Mat4
+	2.000   2.000   2.000   2.000
+	2.000   2.000   2.000   2.000
+	2.000   2.000   2.000   2.000
+	2.000   2.000   2.000   2.000
+
+	Copy Mat4
+	2.000   2.000   2.000   2.000
+	2.000   2.000   2.000   2.000
+	2.000   2.000   2.000   2.000
+	2.000   2.000   2.000   2.000
+
+	Identity * scalar
+	0       0       0       0
+	0       5.000   0       0
+	0       0       10.000  0
+	0       0       0       15.000
+	*/
