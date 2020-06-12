4175e474e425625372349bc1d898fb13cdceb640
LucasGobbs <llcostagobbi@gmail.com>
2020-06-12 18:27:33 -0300

    adicionando mais testes

--- lcg_glal/examples/useful_matrices.c
+++ lcg_glal/examples/useful_matrices.c
@@ -4,8 +4,9 @@
 #include "../glal.h"
 // gcc -Wall -pedantic -std=c99 -o exe lcg_glal/examples/useful_matrices.c -lm
 // gcc -Wno-missing-braces -Wall -pedantic -std=c99 -o exe glal_example.c -lm
-
-
+void model_view_projection();
+void rotation_matrix();
+void rotation_quaternion();
 int main(){
 	
    // Mat4 persp = Mat4_create_perspective((45), 1, 0.001, 1000);
@@ -17,6 +18,24 @@ int main(){
     Vec3_prints(Vec3_qrotate(Vec4_toVec3(dir),Vec3_create_fromArg(0.0, 1.0, 0.0),20.0), "quaternion");
     //Mat4_print(persp);
     return 0;
+}
+void model_view_projection(){
+    Vec3 camera_pos = Vec3_create_fromArg(-10.0, 0.0, 0.0);
+    Vec3 camera_target = Vec3_create_fromArg(0.0, 0.0, 0.0);
+    Vec3 camera_up = Vec3_create_fromArg(0.0, 1.0, 0.0);
+
+    Mat4 projection = Mat4_create_perspective(45.0, 1920/1080, 0.1, 1000);
+    Mat4 view = Mat4_create_lookAt(camera_pos, camera_target, camera_up);
+    Mat4 model = Mat4_create_rotationX(45.0);
+    
+    
+
+}
+void rotation_matrix(){
+
+}
+void rotation_quaternion(){
+
 }
 
 /* outputs
